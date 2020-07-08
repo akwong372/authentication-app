6c058888f67e48cb32fc33deb9f1dfad7ee245e7
akwong372 <akwong372@gmail.com>
2020-07-08 13:44:49 -0700

    add login route

--- app.js
+++ app.js
@@ -12,11 +12,34 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home');
 });
 
 app.get('/login', (req, res) => {
-    res.render('login')
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+
+    db.UserModel.findOne({email}, (err, user)=>{
+        if (err){
+            console.log(`Error logging in: ${err}`);
+            res.redirect('/login');
+        } else if (user){
+            if (user.password === password){
+                console.log(`Logged in: ${email}`);
+                res.render('secrets');
+            } else {
+                console.log(`Wrong password: ${email}`);
+                res.redirect('/login');
+            }
+        } else {
+            console.log(`No user found: ${email}`);
+            res.redirect('/login');
+        }
+    });
 });
 
 app.get('/register', (req, res) => {
