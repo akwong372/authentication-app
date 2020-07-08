06d86114f0025104b000a4a588e30b8e47d321cb
akwong372 <akwong372@gmail.com>
2020-07-08 14:15:14 -0700

    add basic encryption

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+const secret = process.env.SECRET;
 
-mongoose.connect('mongodb://localhost:27017/userDB', {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/userDB', { useUnifiedTopology: true, useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+userSchema.plugin(encrypt, { secret, encryptedFields: ['password'] });
 
 const UserModel = mongoose.model('User', userSchema);
 
